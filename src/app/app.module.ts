ddc5936c5e05a49ba7fa680bfeb40266605dbc65
Tyler Little <tyler.little1989@gmail.com>
2020-01-07 15:34:50 -0600

    Created CardComponent, GameComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GameComponent } from './game/game.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule
